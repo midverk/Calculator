1bb97b7eabc8762523254d2006fccb6aba464a88
Dmytro Veretenko <dmitriyveretenko2010@gmail.com>
2021-04-18 21:45:16 -0500

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public double Add(double x, double y)
+        {
+            return (x + y);
+        }
+        public double Substract(double x, double y)
+        {
+            return (x - y);
+        }
+        public double Multiply(double x, double y)
+        {
+            return (x * y);
+        }
+        public double Divide(double x, double y)
+        {
+            return (x / y);
+        }
+        public double Power(double x, double y)
+        {
+            double res = x;
+            if (y == 0)
+            {
+                res = 1;
+            }
+            else if (y == 1)
+            {
+                return res;
+            }
+            else
+            {
+                for (int i = 1; i < y; i++)
+                {
+                    res *= x;
+                }
+            }
+            return res;
+        }
+        public int Factorial(int x)
+        {
+            int result = 1;
+            if(x == 0)
+            {
+                result = 1;
+            }
+            else
+            {
+                for(int i = 1; i <= x; i++)
+                {
+                    result *= i;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator Math = new Calculator();
+            double add = Math.Add(3, 4);
+            double Sub = Math.Substract(3, 4);
+            double Mul = Math.Multiply(3, 4);
+            double Div = Math.Divide(3, 4);
+            double Pow = Math.Power(3, 0);
+            int Fact = Math.Factorial(5);
+
+            Console.WriteLine(add);
+            Console.WriteLine(Sub);
+            Console.WriteLine(Mul);
+            Console.WriteLine(Div);
+            Console.WriteLine(Pow);
+            Console.WriteLine(Fact);
+        }
+    }
+}
